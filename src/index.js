cc55dc58b8c9a1270ab7579f156e2e3694f1ea52
Medipally, Chetan Kumar <chetankumar.medipally@legato.com>
2022-03-15 17:17:16 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./components/Card"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <div>
+    <h1>My Contacts</h1>
+    <Card name="Beyonce" img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg" 
+    tel="+123 456 789" email="b@beyonce.com"/>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <Card name="Jack Bauer" img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg" 
+        tel="+987 654 321" email="jack@nowhere.com"/>
+
+    <Card name="Chuck Norris" img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png" 
+        tel="+918 372 574" email="gmail@chucknorris.com"/>
+  </div>,
+  document.getElementById("root")
+);
